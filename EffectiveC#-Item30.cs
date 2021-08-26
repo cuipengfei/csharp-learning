fe4df6c436352aea70f806378b5f9ddcb832d350
fugang cui <fgcui1204@163.com>
2021-08-26 18:43:11 +0800

    simple version of nested for looping

--- EffectiveC#-Item30.cs
+++ EffectiveC#-Item30.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -40,7 +41,32 @@ namespace System.Linq
         [Fact]
         public void CompareQueryStatementAndNestedForLoop()
         {
-            
+            var resultFromLoop = BuildTupleWithLoop();
+            var resultFromQueryStatement = BuildTupleWithQueryStatement();
+
+            resultFromLoop.Should().Equal(resultFromQueryStatement);
+        }
+
+        private static IEnumerable<Tuple<int, int>> BuildTupleWithQueryStatement()
+        {
+            return from x in Enumerable.Range(0, 10)
+                from y in Enumerable.Range(0, 10)
+                where x + y < 10
+                select Tuple.Create(x, y);
+        }
+
+        private static IEnumerable<Tuple<int, int>> BuildTupleWithLoop()
+        {
+            for (var x = 0; x < 10; x++)
+            {
+                for (var y = 0; y < 10; y++)
+                {
+                    if (x + y < 10)
+                    {
+                        yield return Tuple.Create(x, y);    
+                    }
+                }
+            }
         }
     }
 
