f6719ab7ce1b53e9f415b5c255acd922bef5cc98
cuipengfei <cuipengfei2008@gmail.com>
2021-09-02 15:56:01 +0800

    49

---
+++ EffectiveC#-Item49.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace csharp_learning
+{
+    public class UnitTest49
+    {
+        [Fact]
+        public void WhenFilterForCatchClause()
+        {
+            int number = 100;
+            bool didCatchRun = false;
+            try
+            {
+                throw new InvalidCastException();
+            }
+            catch (InvalidCastException ex) when (number < 200)
+            {
+                didCatchRun = true;
+            }
+
+            Assert.True(didCatchRun);
+        }
+    }
+}
